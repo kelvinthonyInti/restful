a292afe37fe36e691aaadc9c647ce5326fef85ad
kelvin.meza@intitecperu.com <kelvin-+ja4ever>
2018-10-12 16:30:32 -0500

    Token y api rest para FIT

--- controllers/main.py
+++ controllers/main.py
@@ -58,6 +58,12 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @http.route('/api/kelvin.meza', type='http', auth='none', methods=["GET"], csrf=False)
+    def search_read_res_partner_category(self):
+        return "Kelvin Thony"
+
+
+    @validate_token
     @http.route('/api/res.partner.category', type='http', auth='none', methods=["GET"], csrf=False)
     def search_read_res_partner_category(self):
         kwargs = {"fields": ["id", "name"]}
@@ -446,3 +452,135 @@ class RestApi(http.Controller):
             return 0
 
 
+class APIController(http.Controller):
+    """."""
+
+    def __init__(self):
+        self._model = 'ir.model'
+
+    @validate_token
+    @http.route(_routes, type='http', auth="none", methods=['GET'], csrf=False)
+    def get(self, model=None, id=None, **payload):
+        ioc_name = model
+        model = request.env[self._model].sudo().search(
+            [('model', '=', model)], limit=1)
+        if model:
+            domain, fields, offset, limit, order = extract_arguments(
+                payload)
+            data = request.env[model.model].sudo().search_read(
+                domain=domain, fields=fields, offset=offset, limit=limit, order=order)
+            if data:
+                return valid_response(data)
+            else:
+                return valid_response(data)
+        return invalid_response('invalid object model', 'The model %s is not available in the registry.' % ioc_name)
+
+    @validate_token
+    @http.route(_routes, type='http', auth="none", methods=['POST'], csrf=False)
+    def create(self, model=None, id=None, **payload):
+        """Create a new record.
+        Basic sage:
+        import requests
+
+        headers = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'charset': 'utf-8',
+            'access-token': 'access_token'
+        }
+        data = {
+            'name': 'Babatope Ajepe',
+            'country_id': 105,
+            'child_ids': [
+                {
+                    'name': 'Contact',
+                    'type': 'contact'
+                },
+                {
+                    'name': 'Invoice',
+                   'type': 'invoice'
+                }
+            ],
+            'category_id': [{'id': 9}, {'id': 10}]
+        }
+        req = requests.post('%s/api/res.partner/' %
+                            base_url, headers=headers, data=data)
+
+        """
+        ioc_name = model
+        model = request.env[self._model].sudo().search(
+            [('model', '=', model)], limit=1)
+        if model:
+            try:
+                resource = request.env[model.model].sudo().create(payload)
+            except Exception as e:
+                return invalid_response('params', e)
+            else:
+                data = {'id': resource.id}
+                if resource:
+                    return valid_response(data)
+                else:
+                    return valid_response(data)
+        return invalid_response('invalid object model', 'The model %s is not available in the registry.' % ioc_name)
+
+    @validate_token
+    @http.route(_routes, type='http', auth="none", methods=['PUT'], csrf=False)
+    def put(self, model=None, id=None, **payload):
+        """."""
+        try:
+            _id = int(id)
+        except Exception as e:
+            return invalid_response('invalid object id', 'invalid literal %s for id with base ' % id)
+        _model = request.env[self._model].sudo().search(
+            [('model', '=', model)], limit=1)
+        if not _model:
+            return invalid_response('invalid object model', 'The model %s is not available in the registry.' % model,
+                                    404)
+        try:
+            request.env[_model.model].sudo().browse(_id).write(payload)
+        except Exception as e:
+            return invalid_response('exception', e.name)
+        else:
+            return valid_response('update %s record with id %s successfully!' % (_model.model, _id))
+
+    @validate_token
+    @http.route(_routes, type='http', auth="none", methods=['DELETE'], csrf=False)
+    def delete(self, model=None, id=None, **payload):
+        """."""
+        try:
+            _id = int(id)
+        except Exception as e:
+            return invalid_response('invalid object id', 'invalid literal %s for id with base ' % id)
+        try:
+            record = request.env[model].sudo().search([('id', '=', _id)])
+            if record:
+                record.unlink()
+            else:
+                return invalid_response('missing_record', 'record object with id %s could not be found' % _id, 404)
+        except Exception as e:
+            return invalid_response('exception', e.name, 503)
+        else:
+            return valid_response('record %s has been successfully deleted' % record.id)
+
+    @validate_token
+    @http.route(_routes, type='http', auth="none", methods=['PATCH'], csrf=False)
+    def patch(self, model=None, id=None, action=None, **payload):
+        """."""
+        try:
+            _id = int(id)
+        except Exception as e:
+            return invalid_response('invalid object id', 'invalid literal %s for id with base ' % id)
+        try:
+            record = request.env[model].sudo().search([('id', '=', _id)])
+            _callable = action in [method for method in dir(
+                record) if callable(getattr(record, method))]
+            if record and _callable:
+                # action is a dynamic variable.
+                getattr(record, action)()
+            else:
+                return invalid_response('missing_record',
+                                        'record object with id %s could not be found or %s object has no method %s' % (
+                                            _id, model, action), 404)
+        except Exception as e:
+            return invalid_response('exception', e, 503)
+        else:
+            return valid_response('record %s has been successfully patched' % record.id)
