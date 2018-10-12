8c8fb08938f8209d9a0c830548e681a248a0b8e8
kelvin.meza@intitecperu.com <kelvin-+ja4ever>
2018-10-12 17:05:44 -0500

    Token y api rest para FIT

--- controllers/main.py
+++ controllers/main.py
@@ -80,10 +80,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/einvoice.catalog.type.person', auth='none', methods=["GET"], csrf=False)
     def search_read_einvoice_catalog_type_person(self):
         kwargs = {"fields": ["id", "code", "name"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["einvoice.catalog.type.person"].search_read(**kwargs)
 
     """
@@ -91,10 +92,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/einvoice.catalog.06', auth='none', methods=["GET"], csrf=False)
     def search_read_einvoice_catalog_06(self):
         kwargs = {"fields": ["id", "code", "name"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["einvoice.catalog.06"].search_read(**kwargs)
 
     """
@@ -102,10 +104,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.country', auth='none', methods=["GET"], csrf=False)
     def search_read_res_country(self):
         kwargs = {"fields": ["id", "name", "code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.country"].search_read(**kwargs)
 
     """
@@ -113,10 +116,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.country.state', auth='none', methods=["GET"], csrf=False)
     def search_read_res_country_state(self):
         kwargs = {"fields": ["id", "country_id", "name", "code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.country.state"].search_read(**kwargs)
 
     """
@@ -124,10 +128,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.country.province', auth='none', methods=["GET"], csrf=False)
     def search_read_res_country_province(self):
         kwargs = {"fields": ["id", "state_id", "name", "code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.country.province"].search_read(**kwargs)
 
     """
@@ -135,10 +140,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.country.district', auth='none', methods=["GET"], csrf=False)
     def search_read_res_country_district(self):
         kwargs = {"fields": ["id", "province_id", "name", "code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.country.district"].search_read(**kwargs)
 
     """
@@ -146,10 +152,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/einvoice.catalog.01', auth='none', methods=["GET"], csrf=False)
     def search_read_einvoice_catalog_01(self):
         kwargs = {"fields": ["id", "code", "name", "type_operations_ids"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["einvoice.catalog.01"].search_read(**kwargs)
 
     """
@@ -157,10 +164,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/einvoice.branch.office', auth='none', methods=["GET"], csrf=False)
     def search_read_einvoice_branch_office(self):
         kwargs = {"fields": ["id", "name", "description", "address", "district"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["einvoice.branch.office"].search_read(**kwargs)
 
     """
@@ -169,10 +177,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/einvoice.series.correlative', auth='none', methods=["GET"], csrf=False)
     def search_read_einvoice_series_correlative(self):
         kwargs = {"fields": ["id", "description", "series", "correlative", "branch_office", "catalog_01_id"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["einvoice.series.correlative"].search_read(**kwargs)
 
     """
@@ -180,10 +189,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/account.account', auth='none', methods=["GET"], csrf=False)
     def search_read_account_account(self):
         kwargs = {"fields": ["id", "name", "code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["account.account"].search_read(**kwargs)
 
     """
@@ -192,10 +202,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/product.template', auth='none', methods=["GET"], csrf=False)
     def search_read_product_template(self):
         kwargs = {"fields": ["id", "name", "list_price", "standard_price", "categ_id", "taxes_id"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["product.template"].search_read(**kwargs)
 
     """
@@ -203,10 +214,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/create.validate.partner', auth='none',
                 methods=["POST"], csrf=False)
     def create_validate_partner(self, **kwargs):
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         partner = request.env["res.partner"].search([("vat", "=", kwargs["vat"])])
         idPartner = False
         if len(partner) > 0:
@@ -222,10 +234,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/helpdesk.team', auth='none', methods=["GET"], csrf=False)
     def search_read_helpdesk_team(self):
         kwargs = {"fields": ["id", "name"], "domain": [('it_is_support', '=', True)]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["helpdesk.team"].search_read(**kwargs)
 
     """
@@ -233,10 +246,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/helpdesk.tag', auth='none', methods=["GET"], csrf=False)
     def search_read_helpdesk_tag(self):
         kwargs = {"fields": ["id", "name", "it_invited_ids"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["helpdesk.tag"].search_read(**kwargs)
 
     """
@@ -244,10 +258,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/helpdesk.ticket.type', auth='none', methods=["GET"], csrf=False)
     def search_read_helpdesk_ticket_type(self):
         kwargs = {"fields": ["id", "name"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["helpdesk.ticket.type"].search_read(**kwargs)
 
     """
@@ -255,10 +270,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/it.ticket.origin', auth='none', methods=["GET"], csrf=False)
     def search_read_it_ticket_origin(self):
         kwargs = {"fields": ["id", "it_name", "it_code"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["it.ticket.origin"].search_read(**kwargs)
 
     """
@@ -266,6 +282,7 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/ticket.priority', auth='none', methods=["GET"], csrf=False)
     def search_priority(self):
         array_priority = [
@@ -296,12 +313,13 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.partner', auth='none', methods=["GET"], csrf=False)
     def search_read_res_partner(self):
         kwargs = {
             "fields": ["id", "name", "firstname", "lastname", "lastname2", "vat", "street", "phone", "email", "mobile",
                        "category_id"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.partner"].search_read(**kwargs)
 
     """
@@ -309,10 +327,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/res.users', auth='none', methods=["GET"], csrf=False)
     def search_read_res_users(self):
         kwargs = {"fields": ["id", "email", "partner_id", "name", "login"], "domain": [('id', '!=', 1)]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["res.users"].search_read(**kwargs)
 
     """
@@ -320,10 +339,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/helpdesk.ticket', auth='none',
                 methods=["POST"], csrf=False)
     def create_helpdesk_ticket(self, **kwargs):
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         ticket_id = request.env["helpdesk.ticket"].create(kwargs)
         if ticket_id.id is not False:
             tag_ids = ticket_id.tag_ids
@@ -350,10 +370,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/account.invoice', auth='none',
                 methods=["POST"], csrf=False)
     def create_account_invoice(self, **kwargs):
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         account_invoice_id = request.env["account.invoice"].create(kwargs)
         if account_invoice_id.id is not False:
             for item in kwargs["invoice_lines"]:
@@ -369,18 +390,20 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/account.journal', auth='none', methods=["GET"], csrf=False)
     def search_account_payment(self):
         kwargs = {
             "fields": ["id", "name"]}
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         return request.env["account.journal"].search_read(**kwargs)
 
     @validate_token
+    @make_response()
     @http.route('/api/account.payment', auth='none',
                 methods=["POST"], csrf=False)
     def account_payment(self, **kwargs):
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         if 'invoice_id' in kwargs:
             account_id = request.env["account.invoice"].browse(kwargs["invoice_id"])
             payment_method_id = request.env["account.payment.method"].search(
@@ -409,10 +432,11 @@ class RestApi(http.Controller):
     """
 
     @validate_token
+    @make_response()
     @http.route('/api/create.partner.ticket', auth='none',
                 methods=["POST"], csrf=False)
     def create_partner_ticket(self, **kwargs):
-        # eval_request_params(kwargs)
+        eval_request_params(kwargs)
         partner = request.env["res.partner"].search([("email", "=", kwargs["email"])])
         idPartner = False
         if len(partner) > 0:
